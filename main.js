cca5da95176a97428685bd5623b42816b55c4c07
Marvin Yan <marvin.yan@outlook.com>
2018-07-24 23:19:09 -0400

    Add Coin POJO and eslint files

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
+import Coin from './models/coin';
+
 const EXCLUSIONS = new Set(['Tether']);
 const MAX_QUANTITY = 20;
 
 // Remove existing allocations
-$$('.remove').forEach(el => el.click());
+document.querySelectorAll('remove').forEach(el => el.click());
 
 // Get array of coins
-let possAllocs = document.querySelector('.possible-allocations');
+const possAllocs = document.querySelector('.possible-allocations');
 let coins = possAllocs.querySelectorAll('.item');
-coins = Array.prototype.slice.call(coins)
+coins = Array.prototype.slice.call(coins);
 
 // Sort coins by rank
 coins.sort((a, b) => {
-  const rankA = a.querySelector('.item-info-row > .item-info-row-item:first-of-type > .item-info-row-item-content');
-  const rankB = b.querySelector('.item-info-row > .item-info-row-item:first-of-type > .item-info-row-item-content');
-  return parseInt(rankA.innerHTML) - parseInt(rankB.innerHTML);
+  const rankA = a.querySelector(
+    '.item-info-row > .item-info-row-item:first-of-type > .item-info-row-item-content'
+  );
+  const rankB = b.querySelector(
+    '.item-info-row > .item-info-row-item:first-of-type > .item-info-row-item-content'
+  );
+  return parseInt(rankA.innerHTML, 10) - parseInt(rankB.innerHTML, 10);
 });
 
 // Select the top 20 coins
---
+++ models/coin.js
@@ -0,0 +1,7 @@
+export default class Coin {
+  constructor(name, symbol, marketCap) {
+    this.name = name;
+    this.symbol = symbol;
+    this.marketCap = marketCap;
+  }
+}
