56b882f85d281c0cebfd33cfe6ad441265c50d4f
Marvin Yan <marvin.yan@outlook.com>
2018-07-25 02:03:34 -0400

    Revert back to using querySelector

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name Shrimpy Top 20 Allocator
-// @namespace    http://tampermonkey.net/
+// @name         Shrimpy Top 20 Allocator
 // @version      0.1
 // @description  Enhances Shrimpy.io by providing market cap based rebalancing.
 // @author       Marvin Yan
 // @match        https://www.shrimpy.io/dashboard
 // @grant        none
+// @namespace https://greasyfork.org/users/128831
 // ==/UserScript==
 
 (() => {
@@ -37,18 +37,21 @@
   const addCustomBtn = () => {
     const div = document.createElement('div');
     div.innerHTML = ALLOCATE_BTN_HTML.trim();
-    const buttonsBar = document.getElementsByClassName('shrimpy-card-buttons')[0];
+    const buttonsBar = document.querySelector('.shrimpy-card-buttons');
+    div.firstChild.addEventListener('click', performAllocation);
     buttonsBar.insertAdjacentElement('afterbegin', div.firstChild);
   };
 
+  // Remove existing allocations
   const removeAllocations = () => {
-    // Remove existing allocations
-    document.getElementsByClassName('.remove').forEach(el => el.click());
+    document.querySelectorAll('.remove').forEach(el => el.click());
   };
 
+  // Select the top 20 coins
   const selectTopCoins = () => {
     // Get array of coins
-    let coins = possibleAllocsDiv.getElementsByClassName('.item');
+    const possibleAllocsDiv = document.querySelector('.possible-allocations');
+    let coins = possibleAllocsDiv.querySelectorAll('.item');
     coins = Array.prototype.slice.call(coins);
 
     // Sort coins by rank
@@ -62,15 +65,19 @@
       return parseInt(rankA.innerHTML, 10) - parseInt(rankB.innerHTML, 10);
     });
 
-    // Select the top 20 coins
     let count = 0;
     for (let i = 0; count < MAX_QUANTITY; i++) {
-      const coinName = coins[i].getElementsByClassName('.currency-name')[0].innerHTML.trim();
+      const coinName = coins[i].querySelector('.currency-name').innerHTML.trim();
       if (!EXCLUSIONS.has(coinName)) {
         coins[i].click();
         count++;
       }
     }
+  };
+
+  const performAllocation = () => {
+    removeAllocations();
+    selectTopCoins();
   };
 
   // POJO for standardizing CMC API response
